3f9423460809d9a894534f803ba79ba5f2c60a13
yansong.wang <yansong.wang@carthunder.com>
2024-10-31 17:53:04 +0800

    feat: add product module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '@src/prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [PrismaModule, UserModule],
+  imports: [PrismaModule, UserModule, ProductModule],
 })
 export class AppModule { }
---
+++ src/product/entities/product.entity.ts
@@ -0,0 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { $Enums, product } from "@prisma/client";
+
+export class Product implements product {
+  @ApiProperty() name: string;
+  @ApiProperty() id: number;
+  @ApiProperty() create_time: Date;
+  @ApiProperty() update_time: Date;
+  @ApiProperty() picture: string;
+  @ApiProperty() price: number;
+  @ApiProperty() store_count: number;
+  @ApiProperty() status: $Enums.product_sattus;
+}
+
