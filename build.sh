f14730cb08a551ca6f1a8541770d190a32940a38
yansong.wang <yansong.wang@carthunder.com>
2024-11-15 15:09:41 +0800

    fix : add ci

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ git_hash=$(git rev-parse --short=8 HEAD)
 registry="registry.cn-hangzhou.aliyuncs.com/wyswill_docker"
 pkg_name="kai_bai_user_service"
 
-docker_tag="$registry/$pkg_name:$git_hash"
+export docker_tag="$registry/$pkg_name:$git_hash"
 echo "docker_tag: $docker_tag"
 function build_img() {
   echo "build img"
